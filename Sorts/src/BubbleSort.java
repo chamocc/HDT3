2b7cb30262292872cdda264dc5b0db7d6a202dba
diaz1202 <alejodiaz.1202@gmail.com>
2014-07-25 07:52:22 -0600

    bubbleSort

--- Sorts/src/BubbleSort.java
+++ Sorts/src/BubbleSort.java
@@ -10,4 +10,29 @@
  */
 public class BubbleSort {
     
+    
+    public static void bubbleSort(int data[], int n)
+    // pre: 0 <= n <= data.length
+    // post: values in data[0..n-1] in ascending order
+    {
+        int numSorted = 0; // number of values in order
+        int index; // general index
+        int swap;
+        while (numSorted < n)
+            {
+    // bubble a large element to higher array index
+                for (index = 1; index < n-numSorted; index++)
+                {
+                    if (data[index-1] > data[index])
+                    {
+                       swap = data[index];
+                       data[index]=data[index+1];
+                       data[index+1]=swap;
+                    }
+                    
+                }
+            // at least one more value in place
+            numSorted++;
+            }
+    }
 }
