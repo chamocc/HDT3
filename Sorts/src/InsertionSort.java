ef399c8545a669cc7a0d9094c8c32da9418b5a64
diaz1202 <alejodiaz.1202@gmail.com>
2014-07-25 07:45:14 -0600

    InsertionSort

--- Sorts/src/InsertionSort.java
+++ Sorts/src/InsertionSort.java
@@ -9,5 +9,33 @@
  * @author Daniel
  */
 public class InsertionSort {
-    
+   
+    public static void insertionSort(int data[], int n)
+    // pre: 0 <= n <= data.length
+    // post: values in data[0..n-1] are in ascending order
+    {
+        int numSorted = 1; // number of values in place
+        int index; // general index
+        while (numSorted < n)
+            {
+                // take the first unsorted value
+                 int temp = data[numSorted];
+                // ...and insert it among the sorted:
+                for (index = numSorted; index > 0; index--)
+                {
+                    if (temp < data[index-1])
+                    {
+                        data[index] = data[index-1];
+                    } 
+                    else 
+                    {
+                        break;
+                    }
+                }
+                 // reinsert value
+                data[index] = temp;
+                numSorted++;
+            }
+    }
+
 }
