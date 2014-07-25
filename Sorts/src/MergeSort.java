09b803d2a68caa665955c4f99c070730c32090cc
diaz1202 <alejodiaz.1202@gmail.com>
2014-07-25 07:56:44 -0600

    Sorts

--- Sorts/src/MergeSort.java
+++ Sorts/src/MergeSort.java
@@ -10,4 +10,33 @@
  */
 public class MergeSort {
     
+    private static void merge(int data[], int temp[],int low, int middle, int high)
+        // pre: data[middle..high] are ascending
+        // temp[low..middle-1] are ascending
+        // post: data[low..high] contains all values in ascending order
+    {
+        int ri = low; // result index
+        int ti = low; // temp index
+        int di = middle; // destination index
+        // while two lists are not empty merge smaller value
+        while (ti < middle && di <= high)
+        {
+            if (data[di] < temp[ti])
+            {
+                data[ri++] = data[di++]; // smaller is in high data
+            }
+            else 
+            {
+                data[ri++] = temp[ti++]; // smaller is in temp
+            }
+        }
+        // possibly some values left in temp array
+        while (ti < middle)
+        {
+            data[ri++] = temp[ti++];
+        }
+        // ...or some values left (in correct place) in data array
+    }
+        
+    
 }
--- Sorts/src/QuickSort.java
+++ Sorts/src/QuickSort.java
@@ -10,4 +10,21 @@
  */
 public class QuickSort {
     
+    private static int partition(int data[], int left, int right)
+// pre: left <= right
+// post: data[left] placed in the correct (returned) location
+{
+while (true)
+{
+// move right "pointer" toward left
+while (left < right && data[left] < data[right]) right--;
+if (left < right) swap(data,left++,right);
+else return left;
+// move left pointer toward right
+while (left < right && data[left] < data[right]) left++;
+if (left < right) swap(data,left,right--);
+else return right;
+}
+}
+    
 }
