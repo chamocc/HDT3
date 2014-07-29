f106462705979b355c0c10e56c49a416bec4b988
chamocc <chamocc@gmail.com>
2014-07-28 22:24:01 -0600

    tiempos con el merge sort

--- Sorts/src/Main.java
+++ Sorts/src/Main.java
@@ -28,11 +28,11 @@ public class Main {
       try {
          // Apertura del fichero y creacion de BufferedReader para poder
          // hacer una lectura comoda (disponer del metodo readLine()).
-         archivo = new File ("C:\\Users\\Ed. Chamo\\Desktop\\Trabajos GitHub\\HDT3\\FileGenerator\\datos2000.txt");
+         archivo = new File ("C:\\Users\\Ed. Chamo\\Desktop\\Trabajos GitHub\\HDT3\\FileGenerator\\datos1800.txt");
          fr = new FileReader (archivo);
          br = new BufferedReader(fr);
-         lista= new String[2000];
-         listaInt = new int[2000];
+         lista= new String[1800];
+         listaInt = new int[1800];
  
          // Lectura del fichero
          String linea;
